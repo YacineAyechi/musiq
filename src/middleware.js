506081dbfd7f0ed98d3b901aaf04956c47695221
YacineAyechi <yacineayachi9@gmail.com>
2024-07-06 15:36:57 +0100

    clerk

--- src/middleware.js
+++ src/middleware.js
@@ -2,21 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-// const isProtectedRoute = createRouteMatcher(["/api(.*)"]);
-const isProtectedRoute = createRouteMatcher(["/s(.*)"]);
-
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
-  }
-
-  if (isProtectedRoute(request)) {
-    auth().protect((has) => {
-      return (
-        has({ permission: "org:sys_memberships:manage" }) ||
-        has({ permission: "org:sys_domains_manage" })
-      );
-    });
   }
 });
 
